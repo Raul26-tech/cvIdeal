f85170684f67d09f9150cda379ad39d83abca99c
Raul26-tech <rauletec2020@gmail.com>
2024-12-02 21:16:53 -0300

    first commit

---
+++ src/framework/http/server.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+
+app.listen(process.env.PORT, () => {
+  console.log(`\n\n SERVER IS RUNNING ON PORT ${process.env.PORT}\n\n`);
+});
+
+app.on("error", () => {
+  console.log("\n\n   FAILURE AN UNEXPECTED ERROR OCCURRED  \n\n");
+});
